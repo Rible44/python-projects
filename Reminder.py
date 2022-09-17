759c3080cb7fd7fdd8d3bd29e23b14406535e8d4
Aditya Upadhyay <104327376+Rible44@users.noreply.github.com>
2022-09-17 16:40:06 +0530

    Add files via upload

---
+++ Reminder.py
@@ -0,0 +1,10 @@
+import time
+from plyer import notification
+
+if __name__ == '__main__':
+        notification.notify(
+        title = "please do study otherwise I will kill you",
+        message = "A lack of studying often plagues the student body. Studying is an intrinsic part of the learning process; it promotes retention of information and offers invaluable perspective in all subjects. A lack of studying affects the student negatively; moreover, it leads to low grades, low self esteem, and low sense of values.",
+        app_icon = "E:\python\New folder\icon.ico",
+        timeout = 15
+    )
